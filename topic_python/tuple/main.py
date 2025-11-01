6c50c6b54de1e31f60833e455556f58b40ef5798
Danya <sirazeev00@inbox.ru>
2025-11-01 14:07:24 +0300

    Rename topic_python

---
+++ topic_python/tuple/main.py
@@ -0,0 +1,5 @@
+user_roles = ("admin", "me")
+
+user_roles[0] = "sadas"
+
+print(user_roles)
---
+++ topic_python/while/main.py
@@ -0,0 +1,15 @@
+import random
+
+pc_value = random.randint(1, 10)
+
+while True:
+    my_answer = int(input("Enter value -> "))
+    if (my_answer == pc_value):
+        break
+    else:
+        if my_answer > pc_value:
+            print("PC_VALUE < ")
+        else:
+            print("PC_VALUE > ")
+
+print(my_answer)
