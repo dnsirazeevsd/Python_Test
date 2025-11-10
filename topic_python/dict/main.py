be17304ab9ddbc9e330f71677708fd18321136ba
Danya <sirazeev00@inbox.ru>
2025-11-10 17:18:55 +0300

    Add new topic String

--- topic_python/dict/main.py
+++ topic_python/dict/main.py
@@ -1,23 +1,9 @@
-person_1 = {}
+value = [1,2,3,4,5]
 
-person_1["name"] = "Danil"
-person_1["age"] = 25
-person_1["city"] = "SPB"
-person_1["surname"] = "Sirazeev"
-person_1["job"] = "Forsite"
+def multiply_all(*args) -> int:
+    result = 1
+    for x in args:
+        result *= x
+    return result
 
-person_2 = {
-    "name" : "Emil",
-    "age" : 34,
-    "city" : "Aby",
-    "surname" : "Sirazeev",
-    "job" : "programmer"
-}
-
-for item in person_1.keys():
-    print(person_1[item])
-
-print()
-
-for item in person_2.values():
-    print(item)
+print(multiply_all(*value))  # 24
