8119f6b2525ab4e886ac3bd9b98bbe8def59319e
Danya <sirazeev00@inbox.ru>
2025-11-12 18:05:59 +0300

    Первая задачка на LeedCode

--- leedcode/Example_1_Two_Sum.py
+++ leedcode/Example_1_Two_Sum.py
@@ -1,11 +1,12 @@
 test = [1,2,3,4,5,6,7,8,9]
 
-def twoSum(nums: list[int], target: int) -> list[int]:
-        seen = {}
-        for i, num in enumerate(nums):
-            diff = target - num
-            if diff in seen:
-                  return [seen[diff], i]
-            seen[num] = i
+def two_sum(nums : list[int], target : int) -> list[int]:
+    seen = {}
+    
+    for index, num in enumerate(nums):
+        diff = target - num
+        if diff in seen:
+            return [seen[diff], index]
+        seen[num] = index
 
-print(twoSum(test, 9))
+print(two_sum(test, 9))
